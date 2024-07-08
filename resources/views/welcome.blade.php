524c69905d82aafd7e91788b0822d67e50b8aed7
Alessandro Baratè <161752327+alessandro-barate@users.noreply.github.com>
2024-07-08 15:36:21 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/js/app.js')
+    <title>Laravel Template</title>
+</head>
+
+<body>
+    <h1>Laravel Template</h1>
+
+    {{-- Example tag for images --}}
+    {{-- <img src="{{ Vite::asset('resources/img/logo.png') }}" alt=""> --}}
+</body>
+
+</html>
